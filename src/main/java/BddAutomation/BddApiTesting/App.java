f299f6c8c4ebfb9094e192b0003f89c2be950541
tajinder-dhoot <singhtazinder047@gmail.com>
2022-06-22 01:10:32 -0400

    Setup Maven Project

---
+++ src/main/java/BddAutomation/BddApiTesting/App.java
@@ -0,0 +1,13 @@
+package BddAutomation.BddApiTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/BddAutomation/BddApiTesting/AppTest.java
@@ -0,0 +1,20 @@
+package BddAutomation.BddApiTesting;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
